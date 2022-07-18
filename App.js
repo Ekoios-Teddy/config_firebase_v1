57160656c82d3e26466dfcecf1a4a7a1e2246917
Teddy Nguyen <tho.nguyen@ekoios.vnn>
2022-07-18 11:21:15 +0700

    config firebase android

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View} from 'react-native';
 import auth from '@react-native-firebase/auth';
 
 const App = () => {
   auth()
     .signInWithEmailAndPassword('tho@gmail.com', 'tho1998')
     .then(() => {
-      console.log('User account created & signed in!');
+      console.log('User account created & signed in!', Platform.OS);
     })
     .catch(error => {
       if (error.code === 'auth/email-already-in-use') {
